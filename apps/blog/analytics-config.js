9df88caaf41d306cdbf5a3e36c54570da80ee580
Stian Frøystein <stianfroy@gmail.com>
2026-07-13 14:04:28 +0900

    feat(analytics): activate blog measurement (#32)

---
+++ apps/blog/analytics-config.js
@@ -0,0 +1,6 @@
+window.froysteinAnalytics = Object.freeze({
+  enabled: true,
+  websiteId: "c16b73c4-957f-47c6-b7bc-ea8d71d8ad39",
+  scriptUrl: "/_analytics/script.js",
+  hostUrl: "/_analytics",
+});

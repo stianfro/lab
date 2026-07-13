9b36717f33e2c64fd0dfdf67fa8b6e451c805f78
Stian Frøystein <stianfroy@gmail.com>
2026-07-13 11:30:26 +0900

    feat(analytics): connect development site to Umami

---
+++ apps/froystein-jp-dev/analytics-config.js
@@ -0,0 +1,6 @@
+window.froysteinAnalytics = Object.freeze({
+  enabled: true,
+  websiteId: "e34dfd71-7f91-4c29-b8f7-570b216319c9",
+  scriptUrl: "/_analytics/script.js",
+  hostUrl: "/_analytics",
+});
